7b9ad6c9819ace9026b190704adf262596e123c1
Giorgi Tediashvili <personalmailgt@gmail.com>
2024-02-12 23:29:02 +0400

    add solution

--- C#/8kyu/Summation/Program.cs
+++ C#/8kyu/Summation/Program.cs
@@ -1,2 +1,12 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿namespace Summation
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int sum = Solution.summation(8);
+            
+            Console.WriteLine(sum);
+        }
+    }
+}
---
+++ C#/8kyu/Summation/Solution.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Summation
+{
+    public class Solution
+    {
+        public static int summation(int num)
+        {
+            int sum = 0;
+            int increment = 1;
+       
+
+            while (increment != num + 1)
+            {
+                sum += increment;
+                increment++;
+            }
+
+            return sum;
+        }
+    }
+}
